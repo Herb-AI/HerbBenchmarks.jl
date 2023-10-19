c666859d52c1f72615c9b2a51441fc71dc30d51d
Issa Hanou <i.khanou@tudelft.nl>
2023-10-19 10:59:10 +0200

    added start of interpret

---
+++ src/data/PSB2_2021/grammar_char.jl
@@ -0,0 +1,77 @@
+    function char_lowercase(state)
+        if !isempty(state[:char])
+            cha = state[:char][1]
+            state[:char] = state[:char][2:end]
+            state[:char] = [isupper(cha) ? Char(Int(cha) + 32) : cha; state[:char]]
+        end
+        return state
+    end
+
+    function char_uppercase(state)
+        if !isempty(state[:char])
+            cha = state[:char][1]
+            state[:char] = state[:char][2:end]
+            state[:char] = [islower(cha) ? Char(Int(cha) - 32) : cha; state[:char]]
+        end
+        return state
+    end
+
+    function char_iswhitespace(state)
+        if !isempty(state[:char])
+            item = state[:char][1]
+            state[:char] = state[:char][2:end]
+            state[:boolean] = [item == '\n' || item == ' ' || item == '\t'; state[:boolean]]
+        end
+        return state
+    end
+
+    function char_isdigit(state)
+        if !isempty(state[:char])
+            item = state[:char][1]
+            state[:char] = state[:char][2:end]
+            state[:boolean] = [isdigit(item); state[:boolean]]
+        end
+        return state
+    end
+
+    function char_isletter(state)
+        println(state)
+        if !isempty(state[:char])
+            item = state[:char][1]
+            state[:char] = state[:char][2:end]
+            state[:boolean] = [isletter(item); state[:boolean]]
+        end
+        return state
+    end
+
+    function char_fromfloat(state)
+        if !isempty(state[:float])
+            item = state[:float][1]
+            state[:float] = state[:float][2:end]
+            state[:char] = [Char(mod(Int(item), 128)); state[:char]]
+        end
+        return state
+    end
+
+    function char_frominteger(state)
+        if !isempty(state[:integer])
+            item = state[:integer][1]
+            state[:integer] = state[:integer][2:end]
+            state[:char] = [Char(mod(item, 128)); state[:char]]
+        end
+        return state
+    end
+
+    function char_allfromstring(state)
+        if isempty(state[:string])
+            return state
+        else
+            char_list = reverse(state[:string][1])
+            loop_state = popfirst!(state[:string])
+            while !isempty(char_list)
+                loop_state[:char] = [first(char_list); loop_state[:char]]
+                char_list = rest(char_list)
+            end
+            return char_allfromstring(loop_state)
+        end
+    end
---
+++ src/data/PSB2_2021/grammar_gtm.jl
@@ -0,0 +1,86 @@
+    function init_gtm(push_state)
+        return merge(push_state, Dict(
+            :gtm => Dict(
+                :position => 1,
+                :delay => 0,
+                :tracks => [Dict(), Dict(), Dict()],
+                :trace => []
+            )
+        ))
+    end
+
+    function ensure_instruction_map(instr_map)
+        if isa(instr_map, Dict)
+            return instr_map
+        else
+            return merge(Dict(
+                :instruction => "exec_noop",
+                :close => 0,
+                :silent => false,
+                :random_insertion => false,
+                :uuid => JavaUUID.randomUUID()
+            ), instr_map)
+        end
+    end
+
+    function load_track(push_state, track_index, genome)
+        push_state[:gtm][:tracks][track_index] = Dict()
+        for (i, instr_map) in enumerate(genome)
+            push_state[:gtm][:tracks][track_index][i] = ensure_instruction_map(instr_map)
+        end
+        return push_state
+    end
+
+    function dump_track(push_state, track_index)
+        return [instr_map for instr_map in values(push_state[:gtm][:tracks][track_index]) if !isempty(instr_map)]
+    end
+
+    function trace(trace_info, push_state)
+        push_state[:gtm][:trace] = push_state[:gtm][:trace] * [trace_info]
+        return push_state
+    end
+
+    function gtm_left(state)
+        if haskey(state[:gtm], :position)
+            state[:gtm][:position] -= 1
+            return trace("gtm_left", state)
+        end
+        return state
+    end
+
+    function gtm_right(state)
+        if haskey(state[:gtm], :position)
+            state[:gtm][:position] += 1
+            return trace("gtm_right", state)
+        end
+        return state
+    end
+
+    function gtm_inc_delay(state)
+        if haskey(state[:gtm], :delay)
+            state[:gtm][:delay] += 1
+            return trace("gtm_inc_delay", state)
+        end
+        return state
+    end
+
+    function gtm_dec_delay(state)
+        if haskey(state[:gtm], :delay)
+            state[:gtm][:delay] -= 1
+            return trace("gtm_dec_delay", state)
+        end
+        return state
+    end
+
+    function gtm_dub1(state)
+        if haskey(state[:gtm], :position)
+            pos = state[:gtm][:position]
+            delay = state[:gtm][:delay]
+            if haskey(state[:gtm][:tracks][1], pos)
+                state[:gtm][:tracks][0][pos] = state[:gtm][:tracks][1][pos - delay]
+                state[:gtm][:position] += 1
+            end
+            return trace("gtm_dub1", state)
+        end
+        return state
+    end
