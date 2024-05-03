41b5fe921e9434baa151ce7c4536ef47296cc4cd
Sebastijan <sebastijan.dumancic@gmail.com>
2024-05-03 19:34:02 +0200

    new robots deisgn

--- src/data/Robots_2020/robots_primitives.jl
+++ src/data/Robots_2020/robots_primitives.jl
@@ -1,4 +1,6 @@
-mutable struct RobotState
+using MLStyle
+
+struct RobotState
     holds_ball::Int
     robot_x::Int
     robot_y::Int
@@ -7,89 +9,41 @@ mutable struct RobotState
     size::Int
 end
 
-function initState(holds_ball::Int, robot_x::Int, robot_y::Int, ball_x::Int, ball_y::Int, size::Int)
-    return RobotState(holds_ball, robot_x, robot_y, ball_x, ball_y, size)
-end
 
-function returnState(state::RobotState)
-    return Dict{Symbol, Any}(:holds_ball => state.holds_ball,
-                             :robot_x => state.robot_x, 
-                             :robot_y => state.robot_y,
-                             :ball_x => state.ball_x,
-                             :ball_y => state.ball_y,
-                             :size => state.size)
-end
+function interpret(prog::RuleNode, state::RobotState)
+    rule_node = get_rule(prog)
 
-function atTop(state::RobotState)
-    return state.robot_y == 1
-end
-
-function atBottom(state::RobotState)
-    return state.robot_y == state.size
-end
-
-function atLeft(state::RobotState)
-    return state.robot_x == 1
-end
-
-function atRight(state::RobotState)
-    return state.robot_x == state.size
-end
-
-function notAtTop(state::RobotState)
-    return !atTop(state)
-end
-
-function notAtBottom(state::RobotState)
-    return !atBottom(state)
-end
-
-function notAtLeft(state::RobotState)
-    return !atLeft(state)
-end
-
-function notAtRight(state::RobotState)
-    return !atRight(state)
-end
-
-function moveRight(state::RobotState)
-    if notAtRight(state)
-        state.robot_x += 1
+    @match rule_node begin
+        3 => interpret(prog.children[2], interpret(prog.children[1], state)) # (Operation ; Sequence)
+        6 => notAtRight(state) ? RobotState(state.holds_ball, state.robot_x+1, state.robot_y, state.ball_x, state.ball_y, state.size) : state
+        7 => notAtBottom(state) ? RobotState(state.holds_ball, state.robot_x, state.robot_y+1, state.ball_x, state.ball_y, state.size) : state
+        8 => notAtLeft(state) ? RobotState(state.holds_ball, state.robot_x-1, state.robot_y, state.ball_x, state.ball_y, state.size) : state
+        9 => notAtTop(state) ? RobotState(state.holds_ball, state.robot_x, state.robot_y-1, state.ball_x, state.ball_y, state.size) : state
+        10 => state.holds_ball == 1 ? RobotState(0, state.robot_x, state.robot_y, state.robot_x, state.robot_y, state.size) : state
+        11 => can_pickup(state) ? RobotState(1, state.robot_x-1, state.robot_y, state.ball_x, state.ball_y, state.size) : state
+        12 => interpret(condition, state) ? interpret(tbranch, state) : interpret(fbranch, state) 
+        13 => command_while(prog.children[1], prog.children[2], state)
+        14 => state.robot_y == 1
+        15 => state.robot_y == state.size
+        16 => state.robot_x == 1
+        17 => state.robot_x == state.size
+        18 => !(state.robot_y == 1) 
+        19 => !(state.robot_y == state.size)
+        20 => !(state.robot_x == 1)
+        21 => !(state.robot_x == state.size)
+        _ => interpret(prog.children[1], state) # Start operation Transformation ControlStatement
     end
 end
 
-function moveDown(state::RobotState)
-    if notAtBottom(state)
-        state.robot_y += 1
+can_pickup(state::RobotState) = state.holds_ball == 0 && state.robot_x == state.ball_x && state.robot_y == state.ball_y
+
+function command_while(condition::RuleNode, body::RuleNode, state::RobotState) 
+    while interpret(condition, state)
+        state = interpret(body, state)
     end
+    state
 end
 
-function moveLeft(state::RobotState)
-    if notAtLeft(state)
-        state.robot_x -= 1
-    end
-end
-
-function moveUp(state::RobotState)
-    if notAtTop(state)
-        state.robot_y -= 1
-    end
-end
-
-function drop(state::RobotState)
-    if state.holds_ball == 1
-        state.ball_x = state.robot_x
-        state.ball_y = state.robot_y
-        state.holds_ball = 0
-    end
-end
-
-function grab(state::RobotState)
-    if state.holds_ball == 0 && state.robot_x == state.ball_x && state.robot_y == state.ball_y
-        state.holds_ball = 1
-    end
-end
- 
 function Base.show(io::IO, state::RobotState)
     for y in 1:state.size
         row = "";
