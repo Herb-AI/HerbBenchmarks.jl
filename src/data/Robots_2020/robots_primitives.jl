4842c7c8058a4357b9a8d3e1bcb875f50055ebf9
Sebastijan <sebastijan.dumancic@gmail.com>
2024-05-17 16:01:00 +0200

    new interpreter

--- src/data/Robots_2020/robots_primitives.jl
+++ src/data/Robots_2020/robots_primitives.jl
@@ -9,41 +9,59 @@ struct RobotState
     size::Int
 end
 
-function interpret(prog::AbstractRuleNode, example::IOExample)
-    interpret(prog, example.in[:in])
+function interpret(prog::AbstractRuleNode, grammar::ContextSensitiveGrammar, example::IOExample)
+    interpret(prog, get_relevant_tags(grammar), example.in[:in])
 end
 
-function interpret(prog::AbstractRuleNode, state::RobotState)
-    rule_node = get_rule(prog)
+function interpret(prog::AbstractRuleNode, grammartags::Dict{Int,Symbol}, state::RobotState)
+    rule_node =  prog.ind # get_rule(prog)
 
-    @match rule_node begin
-        3 => interpret(prog.children[2], interpret(prog.children[1], state)) # (Operation ; Sequence)
-        6 => !(state.robot_x == state.size) ? RobotState(state.holds_ball, state.robot_x+1, state.robot_y, state.ball_x, state.ball_y, state.size) : state       #moveright
-        7 => !(state.robot_y == state.size) ? RobotState(state.holds_ball, state.robot_x, state.robot_y+1, state.ball_x, state.ball_y, state.size) : state      #moveDown
-        8 => !(state.robot_x == 1) ? RobotState(state.holds_ball, state.robot_x-1, state.robot_y, state.ball_x, state.ball_y, state.size) : state        #moveLeft
-        9 => !(state.robot_y == 1) ? RobotState(state.holds_ball, state.robot_x, state.robot_y-1, state.ball_x, state.ball_y, state.size) : state         #moveUp
-        10 => state.holds_ball == 1 ? RobotState(0, state.robot_x, state.robot_y, state.robot_x, state.robot_y, state.size) : state                 #drop
-        11 => can_pickup(state) ? RobotState(1, state.robot_x, state.robot_y, state.ball_x, state.ball_y, state.size) : state                     # grab
-        12 => interpret(prog.children[1], state) ? interpret(prog.children[2], state) : interpret(prog.children[3], state)                      #If statement 
-        13 => command_while(prog.children[1], prog.children[2], state)              # while loop
-        14 => state.robot_y == 1            #atTop 
-        15 => state.robot_y == state.size   #atBottom 
-        16 => state.robot_x == 1            #atLeft 
-        17 => state.robot_x == state.size   #atRight
-        18 => !(state.robot_y == 1)         #notAtTop
-        19 => !(state.robot_y == state.size)    # notAtBottom
-        20 => !(state.robot_x == 1)             #notAtLeft
-        21 => !(state.robot_x == state.size)    # notAtRight
-        _ => interpret(prog.children[1], state) # Start operation Transformation ControlStatement
+    @match grammartags[rule_node] begin
+        :OpSeq => interpret(prog.children[2], grammartags, interpret(prog.children[1], grammar, state)) # (Operation ; Sequence)
+        :moveRight => !(state.robot_x == state.size) ? RobotState(state.holds_ball, state.robot_x+1, state.robot_y, state.ball_x, state.ball_y, state.size) : state       #moveright
+        :moveDown => !(state.robot_y == state.size) ? RobotState(state.holds_ball, state.robot_x, state.robot_y+1, state.ball_x, state.ball_y, state.size) : state      #moveDown
+        :moveLeft => !(state.robot_x == 1) ? RobotState(state.holds_ball, state.robot_x-1, state.robot_y, state.ball_x, state.ball_y, state.size) : state        #moveLeft
+        :moveUp => !(state.robot_y == 1) ? RobotState(state.holds_ball, state.robot_x, state.robot_y-1, state.ball_x, state.ball_y, state.size) : state         #moveUp
+        :drop => state.holds_ball == 1 ? RobotState(0, state.robot_x, state.robot_y, state.robot_x, state.robot_y, state.size) : state                 #drop
+        :grab => can_pickup(state) ? RobotState(1, state.robot_x, state.robot_y, state.ball_x, state.ball_y, state.size) : state                     # grab
+        :IF => interpret(prog.children[1], grammartags, state) ? interpret(prog.children[2], grammartags, state) : interpret(prog.children[3], grammartags, state)                      #If statement 
+        :WHILE => command_while(prog.children[1], prog.children[2], grammartags, state)              # while loop
+        :atTop => state.robot_y == 1            #atTop 
+        :atBottom => state.robot_y == state.size   #atBottom 
+        :atLeft => state.robot_x == 1            #atLeft 
+        :atRight => state.robot_x == state.size   #atRight
+        :notAtTop => !(state.robot_y == 1)         #notAtTop
+        :notAtBottom => !(state.robot_y == state.size)    # notAtBottom
+        :notAtLeft => !(state.robot_x == 1)             #notAtLeft
+        :notAtRight => !(state.robot_x == state.size)    # notAtRight
+        _ => interpret(prog.children[1], grammartags, state) # Start operation Transformation ControlStatement
     end
+end
+
+"""
+Gets relevant symbol to easily match the rules
+"""
+function get_relevant_tags(grammar::ContextSensitiveGrammar)
+    tags = Dict{Int,Symbol}()
+    for (ind, r) in pairs(grammar.rules)
+        tags[ind] = if typeof(r) == Symbol
+                        r
+                    else
+                        @match r.head begin
+                            :block =>  :OpSeq
+                            :call =>  r.args[1]             
+                            end
+                        end
+                    end
+    return tags
 end
 
 can_pickup(state::RobotState) = state.holds_ball == 0 && state.robot_x == state.ball_x && state.robot_y == state.ball_y
 
-function command_while(condition::RuleNode, body::RuleNode, state::RobotState, max_steps::Int=1000) 
+function command_while(condition::RuleNode, body::RuleNode, grammartags::Dict{Int,Symbol}, state::RobotState, max_steps::Int=1000) 
     counter = max_steps
-    while interpret(condition, state) && counter > 0
-        state = interpret(body, state)
+    while interpret(condition, grammartags, state) && counter > 0
+        state = interpret(body, grammartags, state)
         counter -= 1
     end
     state
@@ -64,3 +82,10 @@ function Base.show(io::IO, state::RobotState)
         println(io, row)
     end
 end
+
+"""
+mygrammartags = get_relevant_tags(grammar_robots)
+mystate = RobotState(0,1,1,2,1,5)
+moveRight = RuleNode(6, [])
+interpret(moveRight, mygrammartags, mystate)
+"""
--- src/data/String_transformations_2020/string_primitives.jl
+++ src/data/String_transformations_2020/string_primitives.jl
@@ -8,45 +8,63 @@ end
  # Initialize the pointer to 1 (not 0, since Julia is 1-indexed)
 StringState(s::String) = StringState(s, 1)
 
-function interpret(prog::AbstractRuleNode, example::IOExample)
-    interpret(prog, example.in[:in]) 
+function interpret(prog::AbstractRuleNode, grammar::ContextSensitiveGrammar, example::IOExample)
+    interpret(prog, get_relevant_tags(grammar), example.in[:in]) 
 end
 
-function interpret(prog::AbstractRuleNode, state::StringState)
+function interpret(prog::AbstractRuleNode, grammartags::Dict{Int,Symbol}, state::StringState)
     rule_node = get_rule(prog)
 
-    @match rule_node begin
-        3 => interpret(prog.children[2], interpret(prog.children[1], state)) # (Operation ; Sequence)
-        6 => StringState(state.str, min(state.pointer + 1, length(state.str))) # moveRight
-        7 => StringState(state.str, max(state.pointer - 1, 1))   # moveLeft
-        8 => StringState(state.str[1:state.pointer-1] * uppercase(state.str[state.pointer]) * state.str[state.pointer+1:end], state.pointer) #MakeUppercase
-        9 => StringState(state.str[1:state.pointer-1] * lowercase(state.str[state.pointer]) * state.str[state.pointer+1:end], state.pointer) #makeLowercase
-        10 => state.pointer < length(state.string) ? StringState(state.str[1:state.pointer-1] * state.str[state.pointer+1:end], state.pointer) : StringState(state.str[1:state.pointer-1] * state.str[state.pointer+1:end], state.pointer-1) #drop
-        11 => interpret(prog.children[1], state) ? interpret(prog.children[2], state) : interpret(prog.children[3], state) # if statement
-        12 => command_while(prog.children[1], prog.children[2], state) # while statement
-        13 => state.pointer == length(state.str) # atEnd
-        14 => state.pointer != length(state.str) # notAtEnd
-        15 => state.pointer == 1 # atStart
-        16 => state.pointer != 1 # notAtStart
-        17 => state.pointer <= length(state.str) && isletter(state.str[state.pointer]) # isLetter
-        18 => state.pointer > length(state.str) || !isletter(state.str[state.pointer]) # isNotLetter
-        19 => state.pointer <= length(state.str) && isuppercase(state.str[state.pointer]) # isUpperCase 
-        20 => state.pointer > length(state.str) || !isuppercase(state.str[state.pointer]) # isNotUppercase
-        21 => state.pointer <= length(state.str) && islowercase(state.str[state.pointer]) # isLowercase
-        22 => state.pointer > length(state.str) || !islowercase(state.str[state.pointer]) # isNotLowercase
-        23 => state.pointer <= length(state.str) && isdigit(state.str[state.pointer]) # isNumber
-        24 => state.pointer > length(state.str) || !isdigit(state.str[state.pointer]) # isNotNumber
-        25 => state.pointer <= length(state.str) && isspace(state.str[state.pointer]) # isSpace
-        26 => state.pointer > length(state.str) || !isspace(state.str[state.pointer]) # isNotSpace
-        _ => interpret(prog.children[1], state)
+    @match grammartags[rule_node] begin
+        :OpSeq => interpret(prog.children[2],grammartags, interpret(prog.children[1], grammartags, state)) # (Operation ; Sequence)
+        :moveRight => StringState(state.str, min(state.pointer + 1, length(state.str))) # moveRight
+        :moveLeft => StringState(state.str, max(state.pointer - 1, 1))   # moveLeft
+        :makeUppercase => StringState(state.str[1:state.pointer-1] * uppercase(state.str[state.pointer]) * state.str[state.pointer+1:end], state.pointer) #MakeUppercase
+        :makeLowercase => StringState(state.str[1:state.pointer-1] * lowercase(state.str[state.pointer]) * state.str[state.pointer+1:end], state.pointer) #makeLowercase
+        :drop => state.pointer < length(state.string) ? StringState(state.str[1:state.pointer-1] * state.str[state.pointer+1:end], state.pointer) : StringState(state.str[1:state.pointer-1] * state.str[state.pointer+1:end], state.pointer-1) #drop
+        :IF => interpret(prog.children[1], grammartags, state) ? interpret(prog.children[2], grammartags, state) : interpret(prog.children[3], grammartags, state) # if statement
+        :WHILE => command_while(prog.children[1], prog.children[2], grammartags, state) # while statement
+        :atEnd => state.pointer == length(state.str) # atEnd
+        :notAtEnd => state.pointer != length(state.str) # notAtEnd
+        :atStart => state.pointer == 1 # atStart
+        :notatStart => state.pointer != 1 # notAtStart
+        :isLetter => state.pointer <= length(state.str) && isletter(state.str[state.pointer]) # isLetter
+        :isNotLetter => state.pointer > length(state.str) || !isletter(state.str[state.pointer]) # isNotLetter
+        :isUppercase => state.pointer <= length(state.str) && isuppercase(state.str[state.pointer]) # isUpperCase 
+        :isNotUppercase => state.pointer > length(state.str) || !isuppercase(state.str[state.pointer]) # isNotUppercase
+        :isLowercase => state.pointer <= length(state.str) && islowercase(state.str[state.pointer]) # isLowercase
+        :isNotLowercase => state.pointer > length(state.str) || !islowercase(state.str[state.pointer]) # isNotLowercase
+        :isNumber => state.pointer <= length(state.str) && isdigit(state.str[state.pointer]) # isNumber
+        :isNotNumber => state.pointer > length(state.str) || !isdigit(state.str[state.pointer]) # isNotNumber
+        :isSpace => state.pointer <= length(state.str) && isspace(state.str[state.pointer]) # isSpace
+        :isNotSpace => state.pointer > length(state.str) || !isspace(state.str[state.pointer]) # isNotSpace
+        _ => interpret(prog.children[1], grammartags, state)
     end
 
 end
 
-function command_while(condition::RuleNode, body::RuleNode, state::StringState, max_steps::Int=1000)
+"""
+Gets relevant symbol to easily match the rules
+"""
+function get_relevant_tags(grammar::ContextSensitiveGrammar)
+    tags = Dict{Int,Symbol}()
+    for (ind, r) in pairs(grammar.rules)
+        tags[ind] = if typeof(r) == Symbol
+                        r
+                    else
+                        @match r.head begin
+                            :block =>  :OpSeq
+                            :call =>  r.args[1]             
+                            end
+                        end
+                    end
+    return tags
+end
+
+function command_while(condition::RuleNode, body::RuleNode, grammartags::Dict{Int,Symbol}, state::StringState, max_steps::Int=1000)
     counter = max_steps 
-    while interpret(condition, state) && counter > 0
-        state = interpret(body, state)
+    while interpret(condition, grammartags, state) && counter > 0
+        state = interpret(body, grammartags, state)
         counter -= 1
     end
     state
