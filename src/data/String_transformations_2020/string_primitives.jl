88855fb46b1980b30b94267cd94fcfe12b504779
Pamela Wochner <p.wochner@tudelft.nl>
2024-05-22 14:00:38 +0200

    Fix bugs in string primitives. Make tests work with new interpret function.

--- src/data/String_transformations_2020/string_primitives.jl
+++ src/data/String_transformations_2020/string_primitives.jl
@@ -5,29 +5,29 @@ struct StringState
     pointer::Union{Int,Nothing}
 end
 
- # Initialize the pointer to 1 (not 0, since Julia is 1-indexed)
+# Initialize the pointer to 1 (not 0, since Julia is 1-indexed)
 StringState(s::String) = StringState(s, 1)
 
 function interpret(prog::AbstractRuleNode, grammar::ContextSensitiveGrammar, example::IOExample)
-    interpret(prog, get_relevant_tags(grammar), example.in[:in]) 
+    interpret(prog, get_relevant_tags(grammar), example.in[:in])
 end
 
 function interpret(prog::AbstractRuleNode, grammartags::Dict{Int,Symbol}, state::StringState)
     rule_node = get_rule(prog)
 
     @match grammartags[rule_node] begin
-        :OpSeq => interpret(prog.children[2],grammartags, interpret(prog.children[1], grammartags, state)) # (Operation ; Sequence)
+        :OpSeq => interpret(prog.children[2], grammartags, interpret(prog.children[1], grammartags, state)) # (Operation ; Sequence)
         :moveRight => StringState(state.str, min(state.pointer + 1, length(state.str))) # moveRight
         :moveLeft => StringState(state.str, max(state.pointer - 1, 1))   # moveLeft
         :makeUppercase => StringState(state.str[1:state.pointer-1] * uppercase(state.str[state.pointer]) * state.str[state.pointer+1:end], state.pointer) #MakeUppercase
         :makeLowercase => StringState(state.str[1:state.pointer-1] * lowercase(state.str[state.pointer]) * state.str[state.pointer+1:end], state.pointer) #makeLowercase
-        :drop => state.pointer < length(state.string) ? StringState(state.str[1:state.pointer-1] * state.str[state.pointer+1:end], state.pointer) : StringState(state.str[1:state.pointer-1] * state.str[state.pointer+1:end], state.pointer-1) #drop
+        :drop => state.pointer < length(state.str) ? StringState(state.str[1:state.pointer-1] * state.str[state.pointer+1:end], state.pointer) : StringState(state.str[1:state.pointer-1] * state.str[state.pointer+1:end], state.pointer - 1) #drop
         :IF => interpret(prog.children[1], grammartags, state) ? interpret(prog.children[2], grammartags, state) : interpret(prog.children[3], grammartags, state) # if statement
         :WHILE => command_while(prog.children[1], prog.children[2], grammartags, state) # while statement
         :atEnd => state.pointer == length(state.str) # atEnd
         :notAtEnd => state.pointer != length(state.str) # notAtEnd
         :atStart => state.pointer == 1 # atStart
-        :notatStart => state.pointer != 1 # notAtStart
+        :notAtStart => state.pointer != 1 # notAtStart
         :isLetter => state.pointer <= length(state.str) && isletter(state.str[state.pointer]) # isLetter
         :isNotLetter => state.pointer > length(state.str) || !isletter(state.str[state.pointer]) # isNotLetter
         :isUppercase => state.pointer <= length(state.str) && isuppercase(state.str[state.pointer]) # isUpperCase 
@@ -50,19 +50,19 @@ function get_relevant_tags(grammar::ContextSensitiveGrammar)
     tags = Dict{Int,Symbol}()
     for (ind, r) in pairs(grammar.rules)
         tags[ind] = if typeof(r) == Symbol
-                        r
-                    else
-                        @match r.head begin
-                            :block =>  :OpSeq
-                            :call =>  r.args[1]             
-                            end
-                        end
-                    end
+            r
+        else
+            @match r.head begin
+                :block => :OpSeq
+                :call => r.args[1]
+            end
+        end
+    end
     return tags
 end
 
 function command_while(condition::RuleNode, body::RuleNode, grammartags::Dict{Int,Symbol}, state::StringState, max_steps::Int=1000)
-    counter = max_steps 
+    counter = max_steps
     while interpret(condition, grammartags, state) && counter > 0
         state = interpret(body, grammartags, state)
         counter -= 1
--- test/test_datasets.jl
+++ test/test_datasets.jl
@@ -18,7 +18,7 @@ using HerbBenchmarks.String_transformations_2020
 
     prim_atEnd = RuleNode(13, [])
     prim_notAtEnd = RuleNode(14, [])
-    prim_atSart = RuleNode(15, [])
+    prim_atStart = RuleNode(15, [])
     prim_notAtStart = RuleNode(16, [])
     prim_isLetter = RuleNode(17, [])
     prim_isNotLetter = RuleNode(18, [])
@@ -41,29 +41,34 @@ using HerbBenchmarks.String_transformations_2020
     test_state_8 = StringState("a", 1)
     test_state_9 = StringState("", 0)
 
+    # get tags for grammar
+    tags = get_relevant_tags(grammar_string)
+
     # Test case: multiple character string
-    @test interpret(prim_moveRight, test_state_1) == StringState("abc", 2)
-    @test interpret(prim_moveLeft, test_state_2) == StringState("abc", 2)
-    @test interpret(prim_makeUpperCase, test_state_2) == StringState("abC", 3)
-    @test interpret(prim_makeLowerCase, test_state_3) == StringState("abc", 1)
-    @test interpret(prim_drop, test_state_1) == StringState("bc", 1)
-    @test interpret(prim_atEnd, test_state_2) == true
-    @test interpret(prim_atEnd, test_state_1) == false && interpret(prim_notAtEnd, test_state_1) == true
-    @test interpret(prim_atSart, test_state_1) == true && interpret(prim_notAtStart, test_state_1) == false
-    @test interpret(prim_notAtStart, test_state_4) == true && interpret(prim_notAtEnd, test_state_4) == true
-    @test interpret(prim_isLetter, test_state_1) == true
-    @test interpret(prim_isLetter, test_state_5) == false && interpret(prim_isNotLetter, test_state_5) == true
-    @test interpret(prim_isUppercase, test_state_3) == true && interpret(prim_isNotUpperCase, test_state_3) == false
-    @test interpret(prim_isLowercase, test_state_1) == true && interpret(prim_isNotLowercase, test_state_1) == false
-    @test interpret(prim_isNumber, test_state_6) == true
-    @test interpret(prim_isNotNumber, test_state_1) == true
-    @test interpret(prim_isSpace, test_state_6) == false && interpret(prim_isNotSpace, test_state_6) == true
+    @test interpret(prim_moveRight, tags, test_state_1) == StringState("abc", 2)
+    @test interpret(prim_moveLeft, tags, test_state_2) == StringState("abc", 2)
+    @test interpret(prim_makeUpperCase, tags, test_state_2) == StringState("abC", 3)
+    @test interpret(prim_makeLowerCase, tags, test_state_3) == StringState("abc", 1)
+    @test interpret(prim_drop, tags, test_state_1) == StringState("bc", 1)
+    @test interpret(prim_atEnd, tags, test_state_2) == true
+    @test interpret(prim_atEnd, tags, test_state_1) == false && interpret(prim_notAtEnd, tags, test_state_1) == true
+    @test interpret(prim_atStart, tags, test_state_1) == true
+    @test interpret(prim_notAtStart, tags, test_state_1) == false
+
+    @test interpret(prim_notAtStart, tags, test_state_4) == true && interpret(prim_notAtEnd, tags, test_state_4) == true
+    @test interpret(prim_isLetter, tags, test_state_1) == true
+    @test interpret(prim_isLetter, tags, test_state_5) == false && interpret(prim_isNotLetter, tags, test_state_5) == true
+    @test interpret(prim_isUppercase, tags, test_state_3) == true && interpret(prim_isNotUpperCase, tags, test_state_3) == false
+    @test interpret(prim_isLowercase, tags, test_state_1) == true && interpret(prim_isNotLowercase, tags, test_state_1) == false
+    @test interpret(prim_isNumber, tags, test_state_6) == true
+    @test interpret(prim_isNotNumber, tags, test_state_1) == true
+    @test interpret(prim_isSpace, tags, test_state_6) == false && interpret(prim_isNotSpace, tags, test_state_6) == true
 
     # Test case: single character string
-    @test interpret(prim_moveRight, test_state_8) == StringState("a", 1)
-    @test interpret(prim_moveLeft, test_state_8) == StringState("a", 1)
-    @test interpret(prim_drop, test_state_8) == StringState("", 0) # TODO: is that how we want this to behave?
-    @test interpret(prim_atEnd, test_state_8) == true && interpret(prim_atSart, test_state_8) == true
+    @test interpret(prim_moveRight, tags, test_state_8) == StringState("a", 1)
+    @test interpret(prim_moveLeft, tags, test_state_8) == StringState("a", 1)
+    @test interpret(prim_drop, tags, test_state_8) == StringState("", 0) # TODO: is that how we want this to behave?
+    @test interpret(prim_atEnd, tags, test_state_8) == true && interpret(prim_atStart, tags, test_state_8) == true
 
 end
 
