158af6fb5c8f6ebe6087ce1de5e8039463533b4e
Pamela Wochner <p.wochner@tudelft.nl>
2024-05-24 17:17:45 +0200

    Add tests for String and Robot benchmarks.

--- test/test_datasets.jl
+++ test/test_datasets.jl
@@ -4,6 +4,7 @@ using Test
 
 using HerbBenchmarks
 using HerbBenchmarks.String_transformations_2020
+using HerbBenchmarks.Robots_2020
 
 @testset verbose = true "String transformations 2020" begin
     problems = all_problems(String_transformations_2020)
@@ -42,56 +43,122 @@ using HerbBenchmarks.String_transformations_2020
     test_state_9 = StringState("", 0)
 
     # get tags for grammar
-    tags = get_relevant_tags(grammar_string)
+    tags = String_transformations_2020.get_relevant_tags(grammar_string)
 
     # Test case: multiple character string
-    @test interpret(prim_moveRight, tags, test_state_1) == StringState("abc", 2)
-    @test interpret(prim_moveLeft, tags, test_state_2) == StringState("abc", 2)
-    @test interpret(prim_makeUppercase, tags, test_state_2) == StringState("abC", 3)
-    @test interpret(prim_makeLowercase, tags, test_state_3) == StringState("abc", 1)
-    @test interpret(prim_drop, tags, test_state_1) == StringState("bc", 1)
-    @test interpret(prim_atEnd, tags, test_state_2) == true
-    @test interpret(prim_atEnd, tags, test_state_1) == false && interpret(prim_notAtEnd, tags, test_state_1) == true
-    @test interpret(prim_atStart, tags, test_state_1) == true
-    @test interpret(prim_notAtStart, tags, test_state_1) == false
+    @test String_transformations_2020.interpret(prim_moveRight, tags, test_state_1) == StringState("abc", 2)
+    @test String_transformations_2020.interpret(prim_moveLeft, tags, test_state_2) == StringState("abc", 2)
+    @test String_transformations_2020.interpret(prim_makeUppercase, tags, test_state_2) == StringState("abC", 3)
+    @test String_transformations_2020.interpret(prim_makeLowercase, tags, test_state_3) == StringState("abc", 1)
+    @test String_transformations_2020.interpret(prim_drop, tags, test_state_1) == StringState("bc", 1)
+    @test String_transformations_2020.interpret(prim_atEnd, tags, test_state_2) == true
+    @test String_transformations_2020.interpret(prim_atEnd, tags, test_state_1) == false && String_transformations_2020.interpret(prim_notAtEnd, tags, test_state_1) == true
+    @test String_transformations_2020.interpret(prim_atStart, tags, test_state_1) == true
+    @test String_transformations_2020.interpret(prim_notAtStart, tags, test_state_1) == false
 
-    @test interpret(prim_notAtStart, tags, test_state_4) == true && interpret(prim_notAtEnd, tags, test_state_4) == true
-    @test interpret(prim_isLetter, tags, test_state_1) == true
-    @test interpret(prim_isLetter, tags, test_state_5) == false && interpret(prim_isNotLetter, tags, test_state_5) == true
-    @test interpret(prim_isUppercase, tags, test_state_3) == true && interpret(prim_isNotUppercase, tags, test_state_3) == false
-    @test interpret(prim_isLowercase, tags, test_state_1) == true && interpret(prim_isNotLowercase, tags, test_state_1) == false
-    @test interpret(prim_isNumber, tags, test_state_6) == true
-    @test interpret(prim_isNotNumber, tags, test_state_1) == true
-    @test interpret(prim_isSpace, tags, test_state_6) == false && interpret(prim_isNotSpace, tags, test_state_6) == true
+    @test String_transformations_2020.interpret(prim_notAtStart, tags, test_state_4) == true && String_transformations_2020.interpret(prim_notAtEnd, tags, test_state_4) == true
+    @test String_transformations_2020.interpret(prim_isLetter, tags, test_state_1) == true
+    @test String_transformations_2020.interpret(prim_isLetter, tags, test_state_5) == false && String_transformations_2020.interpret(prim_isNotLetter, tags, test_state_5) == true
+    @test String_transformations_2020.interpret(prim_isUppercase, tags, test_state_3) == true && String_transformations_2020.interpret(prim_isNotUppercase, tags, test_state_3) == false
+    @test String_transformations_2020.interpret(prim_isLowercase, tags, test_state_1) == true && String_transformations_2020.interpret(prim_isNotLowercase, tags, test_state_1) == false
+    @test String_transformations_2020.interpret(prim_isNumber, tags, test_state_6) == true
+    @test String_transformations_2020.interpret(prim_isNotNumber, tags, test_state_1) == true
+    @test String_transformations_2020.interpret(prim_isSpace, tags, test_state_6) == false && String_transformations_2020.interpret(prim_isNotSpace, tags, test_state_6) == true
 
     # Test case: single character string
-    @test interpret(prim_moveRight, tags, test_state_8) == StringState("a", 1)
-    @test interpret(prim_moveLeft, tags, test_state_8) == StringState("a", 1)
-    @test interpret(prim_drop, tags, test_state_8) == StringState("", 0) # TODO: is that how we want this to behave?
-    @test interpret(prim_atEnd, tags, test_state_8) == true && interpret(prim_atStart, tags, test_state_8) == true
+    @test String_transformations_2020.interpret(prim_moveRight, tags, test_state_8) == StringState("a", 1)
+    @test String_transformations_2020.interpret(prim_moveLeft, tags, test_state_8) == StringState("a", 1)
+    @test String_transformations_2020.interpret(prim_drop, tags, test_state_8) == StringState("", 0) # TODO: is that how we want this to behave?
+    @test String_transformations_2020.interpret(prim_atEnd, tags, test_state_8) == true && String_transformations_2020.interpret(prim_atStart, tags, test_state_8) == true
 
     # Test case: empty string (pointer at 0)
     ## Transformations
-    @test interpret(prim_moveRight, tags, test_state_9) == StringState("", 0)
-    @test interpret(prim_moveLeft, tags, test_state_9) == StringState("", 1) # TODO: shouldn't change string state -> == StringState("", 0)
-    @test_throws BoundsError interpret(prim_makeUppercase, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_makeLowercase, tags, test_state_9)
-    @test interpret(prim_drop, tags, test_state_9) == StringState("", -1) # TODO: pointer shouldn't be -1
+    @test String_transformations_2020.interpret(prim_moveRight, tags, test_state_9) == StringState("", 0)
+    @test String_transformations_2020.interpret(prim_moveLeft, tags, test_state_9) == StringState("", 1) # TODO: shouldn't change string state -> == StringState("", 0)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_makeUppercase, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_makeLowercase, tags, test_state_9)
+    @test String_transformations_2020.interpret(prim_drop, tags, test_state_9) == StringState("", -1) # TODO: pointer shouldn't be -1
 
     ## Conditions
-    @test interpret(prim_atEnd, tags, test_state_9) == true
-    # @test interpret(prim_atStart, tags, test_state_9) == true # TODO: should evaluate to true
-    @test interpret(prim_notAtEnd, tags, test_state_9) == false
-    # @test interpret(prim_notAtStart, tags, test_state_9) == true 
-    @test_throws BoundsError interpret(prim_isLetter, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isNotLetter, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isUppercase, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isNotUppercase, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isLowercase, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isNotLowercase, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isNumber, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isNotNumber, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isSpace, tags, test_state_9)
-    @test_throws BoundsError interpret(prim_isNotSpace, tags, test_state_9)
+    @test String_transformations_2020.interpret(prim_atEnd, tags, test_state_9) == true
+    # @test String_transformations_2020.interpret(prim_atStart, tags, test_state_9) == true # TODO: should evaluate to true
+    @test String_transformations_2020.interpret(prim_notAtEnd, tags, test_state_9) == false
+    # @test String_transformations_2020.interpret(prim_notAtStart, tags, test_state_9) == true # TODO
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isLetter, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isNotLetter, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isUppercase, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isNotUppercase, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isLowercase, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isNotLowercase, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isNumber, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isNotNumber, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isSpace, tags, test_state_9)
+    @test_throws BoundsError String_transformations_2020.interpret(prim_isNotSpace, tags, test_state_9)
 end
 
+@testset verbose = true "Robots 2020" begin
+    problems = all_problems(Robots_2020)
+    @test typeof(problems[1]) <: HerbSpecification.Problem
+    @test typeof(problems[1].spec[1]) == HerbSpecification.IOExample
+
+    prim_moveRight = RuleNode(6, [])
+    prim_moveDown = RuleNode(7, [])
+    prim_moveLeft = RuleNode(8, [])
+    prim_moveUp = RuleNode(9, [])
+    prim_drop = RuleNode(10, [])
+    prim_grab = RuleNode(11, [])
+    prim_atTop = RuleNode(14, [])
+    prim_atBottom = RuleNode(15, [])
+    prim_atLeft = RuleNode(16, [])
+    prim_atRight = RuleNode(17, [])
+    prim_notAtTop = RuleNode(18, [])
+    prim_notAtBottom = RuleNode(19, [])
+    prim_notAtLeft = RuleNode(20, [])
+    prim_notAtRight = RuleNode(21, [])
+    prim_if = RuleNode(12, [RuleNode(18), RuleNode(9), RuleNode(7)]) # IF(notAtTop, moveUp, moveDown)
+
+    # get tags for grammar
+    tags = Robots_2020.get_relevant_tags(grammar_robots)
+
+    # Test case: robot is at the top left corner of the grid, not holding ball (ball position shouldn't change)
+    test_state_1 = RobotState(0, 1, 1, 1, 1, 5)
+    @test Robots_2020.interpret(prim_moveRight, tags, test_state_1) == RobotState(0, 2, 1, 1, 1, 5)
+    @test Robots_2020.interpret(prim_moveDown, tags, test_state_1) == RobotState(0, 1, 2, 1, 1, 5)
+    @test Robots_2020.interpret(prim_moveLeft, tags, test_state_1) == RobotState(0, 1, 1, 1, 1, 5)
+    @test Robots_2020.interpret(prim_moveUp, tags, test_state_1) == RobotState(0, 1, 1, 1, 1, 5)
+    @test Robots_2020.interpret(prim_drop, tags, test_state_1) == RobotState(0, 1, 1, 1, 1, 5)
+    @test Robots_2020.interpret(prim_grab, tags, test_state_1) == RobotState(1, 1, 1, 1, 1, 5)
+
+    @test Robots_2020.interpret(prim_atTop, tags, test_state_1) == true
+    @test Robots_2020.interpret(prim_atLeft, tags, test_state_1) == true
+    @test Robots_2020.interpret(prim_notAtBottom, tags, test_state_1) == true
+    @test Robots_2020.interpret(prim_notAtRight, tags, test_state_1) == true
+
+    @test Robots_2020.interpret(prim_atBottom, tags, test_state_1) == false
+    @test Robots_2020.interpret(prim_atRight, tags, test_state_1) == false
+    @test Robots_2020.interpret(prim_notAtTop, tags, test_state_1) == false
+    @test Robots_2020.interpret(prim_notAtLeft, tags, test_state_1) == false
+
+    @test Robots_2020.interpret(prim_if, tags, test_state_1) == RobotState(0, 1, 2, 1, 1, 5)
+
+    # Test case: robot is at the bottom right corner of the grid, holding ball (ball position should change)
+    test_state_2 = RobotState(1, 5, 5, 5, 5, 5)
+    @test Robots_2020.interpret(prim_moveRight, tags, test_state_2) == RobotState(1, 5, 5, 5, 5, 5)
+    @test Robots_2020.interpret(prim_moveDown, tags, test_state_2) == RobotState(1, 5, 5, 5, 5, 5)
+    @test Robots_2020.interpret(prim_moveLeft, tags, test_state_2) == RobotState(1, 4, 5, 4, 5, 5)
+    @test Robots_2020.interpret(prim_moveUp, tags, test_state_2) == RobotState(1, 5, 4, 5, 4, 5)
+    @test Robots_2020.interpret(prim_drop, tags, test_state_2) == RobotState(0, 5, 5, 5, 5, 5)
+    @test Robots_2020.interpret(prim_grab, tags, test_state_2) == RobotState(1, 5, 5, 5, 5, 5)
+
+    @test Robots_2020.interpret(prim_atBottom, tags, test_state_2) == true
+    @test Robots_2020.interpret(prim_atRight, tags, test_state_2) == true
+    @test Robots_2020.interpret(prim_notAtTop, tags, test_state_2) == true
+    @test Robots_2020.interpret(prim_notAtLeft, tags, test_state_2) == true
+
+    @test Robots_2020.interpret(prim_atTop, tags, test_state_2) == false
+    @test Robots_2020.interpret(prim_atLeft, tags, test_state_2) == false
+    @test Robots_2020.interpret(prim_notAtBottom, tags, test_state_2) == false
+    @test Robots_2020.interpret(prim_notAtRight, tags, test_state_2) == false
+
+    @test Robots_2020.interpret(prim_if, tags, test_state_2) == RobotState(1, 5, 4, 5, 4, 5)
+end
