77caecd1197c2ac16ec70baef00bc26a0e843ddb
Issa Hanou <i.khanou@tudelft.nl>
2024-10-09 16:41:43 +0200

    added ERCs and command_while and restructuring of grammars

--- src/data/PSB2_2021/base_grammars.jl
+++ src/data/PSB2_2021/base_grammars.jl
@@ -1,62 +1,60 @@
 include("psb2_primitives.jl")
 
-# TODO parsing of integers, rename Int
 grammar_integer = @csgrammar begin
-    Int = Int + Int
-    Int = Int - Int
-    Int = Int * Int 
-    Int = Int / Int
-    Int = Int % Int 
-    Int = Int ^ Int 
-    Int = Int ^ 2
-    Int = Int + 1
-    Int = Int - 1
-    Int = Int * -1
-    Int = max(Int, Int)
-    Int = min(Int, Int)
-    Int = abs(Int)
-    Int = Integer(Boolean)
-    Int = Integer(String)
-    Int = Integer(Float)
-    Int = Integer(Character)
-    Int = ceil(Int)
-    Int = floor(Int)
-    Boolean = Int > Int
-    Boolean = Int >= Int 
-    Boolean = Int < Int 
-    Boolean = Int <= Int 
-    Boolean = Int == Int 
-    Boolean = Int != Int
-    Int = Boolean ? Int : Int
-    Expression = Int
+    IntRule = IntRule + IntRule
+    IntRule = IntRule - IntRule
+    IntRule = IntRule * IntRule 
+    IntRule = IntRule / IntRule
+    IntRule = IntRule % IntRule 
+    IntRule = IntRule ^ IntRule 
+    IntRule = IntRule ^ 2
+    IntRule = IntRule + 1
+    IntRule = IntRule - 1
+    IntRule = IntRule * -1
+    IntRule = max(IntRule, IntRule)
+    IntRule = min(IntRule, IntRule)
+    IntRule = abs(IntRule)
+    IntRule = Integer(Boolean)
+    IntRule = Integer(String)
+    IntRule = Integer(Float)
+    IntRule = Integer(Character)
+    IntRule = ceil(IntRule)
+    IntRule = floor(IntRule)
+    Boolean = IntRule > IntRule
+    Boolean = IntRule >= IntRule 
+    Boolean = IntRule < IntRule 
+    Boolean = IntRule <= IntRule 
+    Boolean = IntRule == IntRule 
+    Boolean = IntRule != IntRule
+    IntRule = Boolean ? IntRule : IntRule
+    Expression = IntRule
     Expression = begin Expression; Expression end
-    Int = Int -> while Bool; Expression end
+    IntRule = command_while(Boolean, Expression)
 end
 
 grammar_state_integer = @csgrammar begin
-    # TODO need string/float/int/bool?
-    State = Dict(Sym => Int)
-    State = Dict(Sym => Int, Sym => Int)
-    State = Dict(Sym => Int, Sym => Int, Sym => Int)
+    State = Dict(Sym => IntRule)
+    State = Dict(Sym => IntRule, Sym => IntRule)
+    State = Dict(Sym => IntRule, Sym => IntRule, Sym => IntRule)
     State = merge!(state, State)
-    State = push(State, Sym => Int)
-    Int = get(state, Sym, "Key not found")
-    Expression = State | Int
+    State = push(State, Sym => IntRule)
+    IntRule = get(state, Sym, "Key not found")
+    Expression = State | IntRule
     Expression = let state = State; Expression end
 end
 
 grammar_list_integer = @csgrammar begin
     List = map(Func, List)
-    Func = (x -> Int)
-    Int = x
-    Int = length(List)
-    Int = sum(List)
-    Int = indexin(Int, List)
-    List = getindex(List, Int:Int)
+    Func = (x -> IntRule)
+    IntRule = x
+    IntRule = length(List)
+    IntRule = sum(List)
+    IntRule = indexin(IntRule, List)
+    List = getindex(List, IntRule:IntRule)
 end
 
 grammar_float = @csgrammar begin
-    Number = Float | Int
+    Number = Float | IntRule
     Number = Number + Number
     Number = Number - Number
     Number = Number * Number 
@@ -79,16 +77,16 @@ grammar_float = @csgrammar begin
     Float = argcos(Number)
     Float = argsin(Number)
     Float = argtan(Number)
-    Float = log(Number, Int)
+    Float = log(Number, IntRule)
     Float = log(Number, 2)
     Float = log(Number, 10)
-    Float = float(Int)
+    Float = float(IntRule)
     Float = float(String)
     Float = float(Boolean)
     Expression = Float
     Expression = begin Expression; Expression end
     Float = Boolean ? Expression : Expression
-    Float = Float -> while Bool; Expression end
+    Float = command_while(Boolean, Expression)
 end
 
 grammar_boolean = @csgrammar begin
@@ -96,13 +94,13 @@ grammar_boolean = @csgrammar begin
     Boolean = Boolean || Boolean 
     Boolean = !Boolean 
     Boolean = Boolean ⊻ Boolean
-    Boolean = Bool(Int)
+    Boolean = Bool(IntRule)
     Boolean = Bool(Float)
     Boolean = Boolean ? Boolean : Boolean
 end
 
 grammar_character = @csgrammar begin
-    Character = Char(Int)
+    Character = Char(IntRule)
     Character = Char(Float)
     Character = Char(Boolean)
     Character = Char(String)
@@ -116,13 +114,13 @@ end
 
 grammar_string = @csgrammar begin
     String = string(Character)
-    String = string(Int)
+    String = string(IntRule)
     String = string(Boolean)
     String = string(Float)
     String = String * String
     String = String * string(Character)
-    String = String[Int]
-    String = chop(String, head=Int, last=Int)
+    String = String[IntRule]
+    String = chop(String, head=IntRule, last=IntRule)
     String = chop(String, head=0, last=1)
     String = first(String)
     String = last(String)
@@ -134,15 +132,15 @@ grammar_string = @csgrammar begin
     String = replace(String, Character=>"")
     String = uppercase(String)
     String = lowercase(String)
-    String = replace_in_string(String, Int, Character)
+    String = replace_in_string(String, IntRule, Character)
     String = ""
-    Int = length(String)
-    Int = findfirst(Character, String)
-    Int = count(String, String)
+    IntRule = length(String)
+    IntRule = findfirst(Character, String)
+    IntRule = count(String, String)
     Boolean = contains(Character, String)
     Boolean = contains(String, String)
     String = Boolean ? String : String
     Expression = String
     Expression = begin Expression; Expression end
-    String = String -> while Boolean; Expression end
+    String = command_while(Boolean, Expression)
 end
--- src/data/PSB2_2021/example_using_the_benchmark.jl
+++ src/data/PSB2_2021/example_using_the_benchmark.jl
@@ -5,13 +5,13 @@ Pkg.add(["HerbGrammar", "HerbSpecification", "HerbInterpret", "HerbSearch"])
 using HerbGrammar, HerbSpecification, HerbInterpret, HerbSearch
 
 include("data.jl")
-include("grammars.jl")
+include("grammar.jl")
 
 # Define an iterator to use with the correct grammar and starting symbol. Optionally you can define a max_depth
-iterator = BFSIterator(minimal_grammar_fizz_buzz, :String, max_depth=5)
+iterator = BFSIterator(minimal_grammar_basement, :Return, max_depth=10)
 
 # Synthesize the program for the given problem. Evaluation errors must be allowed for some problems to not fail on trying to divide by 0 for example
-program = @time synth(problem_fizz_buzz, iterator, allow_evaluation_errors = true)
+program = @time synth(problem_basement, iterator, allow_evaluation_errors = true)
 
 # Note: this might take quite some time as a BFSIterator is not very good on its own.
 
