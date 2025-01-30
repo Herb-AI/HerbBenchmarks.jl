06ed2f79c2f7f7de7a6d5b86f3aa185b2808def4
Pamela Wochner <p.wochner@tudelft.nl>
2025-01-30 11:37:20 +0100

    Fix test failings on type equality with IOExample

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,10 @@ using HerbSpecification
 using Test
 
 @testset "HerbBenchmarks.jl" verbose = true begin
-    include("test_general.jl")
-    include("test_string_transformations_2020.jl")
-    include("test_robots_2020.jl")
-    include("test_pixels_2020.jl")
-    include("test_abstract_reasoning_2019.jl")
-    include("test_problem_fetcher.jl")
+	include("test_general.jl")
+	include("test_string_transformations_2020.jl")
+	include("test_robots_2020.jl")
+	include("test_pixels_2020.jl")
+	include("test_abstract_reasoning_2019.jl")
+	include("test_problem_fetcher.jl")
 end
