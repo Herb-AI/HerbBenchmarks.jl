c124f1b86f248cfa6cd1a6a47d8083d43ef07747
Tilman Hinnerichs <tilman@hinnerichs.com>
2026-03-19 16:05:39 +0100

    Add ARC_AGI_2 data; update README

--- src/data/ARC_AGI_2/ARC_AGI_2.jl
+++ src/data/ARC_AGI_2/ARC_AGI_2.jl
@@ -1,4 +1,37 @@
 module ARC_AGI_2
 
+using HerbCore
+using HerbSpecification
+using HerbGrammar
+
+include("data.jl")
+# include("grammar.jl")
+
+"""
+    training_problems()
+
+Return the ARC-AGI-2 public training problems.
+"""
+training_problems() = ARC_AGI2_TRAINING
+
+"""
+    evaluation_problems()
+
+Return the ARC-AGI-2 public evaluation problems.
+"""
+evaluation_problems() = ARC_AGI2_EVALUATION
+
+"""
+    all_problems()
+
+Return all public ARC-AGI-2 problems.
+"""
+all_problems() = ARC_AGI2_ALL
+
+
+export 
+    training_problems
+    evaluation_problems
+    all_problems
 
 end # module ARC_AGI_2
