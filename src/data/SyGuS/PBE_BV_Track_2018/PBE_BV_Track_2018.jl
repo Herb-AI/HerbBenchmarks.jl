10f07530c03bd5510f636aa5aa06b66e69ad994b
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2026-03-19 14:20:04 +0100

    feat: switch PBE_BV_Track_2018 to generated interpreter

--- src/data/SyGuS/PBE_BV_Track_2018/PBE_BV_Track_2018.jl
+++ src/data/SyGuS/PBE_BV_Track_2018/PBE_BV_Track_2018.jl
@@ -1,19 +1,20 @@
 module PBE_BV_Track_2018
-
-using RuntimeGeneratedFunctions
-RuntimeGeneratedFunctions.init(@__MODULE__)
-
 using HerbCore
 using HerbSpecification
 using HerbGrammar
+using HerbInterpret
+
+using RuntimeGeneratedFunctions
+RuntimeGeneratedFunctions.init(@__MODULE__)
 
 include("data.jl")
 include("grammars.jl")
 
 include("bit_functions.jl")
 
-export 
-    format_bit_operations_grammars
+function make_bv_interpreter(g)
+    return make_interpreter(g; target_module=PBE_BV_Track_2018, cache_module=PBE_BV_Track_2018)
+end
 
 function format_bit_operations_grammars(filename::AbstractString)
     lines::Vector{String} = []
@@ -22,41 +23,41 @@ function format_bit_operations_grammars(filename::AbstractString)
     for line in eachline(file)
         if !isempty(line) && occursin(" = ", line)
             split_line = split(line, " = ")
-            
+
             # Check for grammar definition
             if occursin("@cfgrammar", split_line[2])
-                line = replace(line, "-" => "_") 
+                line = replace(line, "-" => "_")
             else
-                if startswith(split_line[2], 'x') 
+                if startswith(split_line[2], 'x')
                     line = "$(split_line[1]) = _arg_1"
                 end
-                if startswith(split_line[2], '(') 
+                if startswith(split_line[2], '(')
                     symbol_list = split(split_line[2][2:end-1], ' ')
                     func = replace(symbol_list[1],
-                         "bvneg" => "bvneg_cvc",
-                         "bvnot" => "bvnot_cvc",
-                         "bvadd" => "bvadd_cvc",
-                         "bvsub" => "bvsub_cvc",
-                         "bvxor" => "bvxor_cvc",
-                         "bvand" => "bvand_cvc",
-                         "bvor" => "bvor_cvc",
-                         "bvshl" => "bvshl_cvc",
-                         "bvlshr" => "bvlshr_cvc",
-                         "bvashr" => "bvashr_cvc",
-                         "bvnand" => "bvnand_cvc",
-                         "bvnor" => "bvnor_cvc",
-                         "ehad" => "ehad_cvc",
-                         "arba" => "arba_cvc",
-                         "shesh" => "shesh_cvc",
-                         "smol" => "bvnor_cvc",
-                         "im" => "im_cvc",
-                         )
+                        "bvneg" => "bvneg_cvc",
+                        "bvnot" => "bvnot_cvc",
+                        "bvadd" => "bvadd_cvc",
+                        "bvsub" => "bvsub_cvc",
+                        "bvxor" => "bvxor_cvc",
+                        "bvand" => "bvand_cvc",
+                        "bvor" => "bvor_cvc",
+                        "bvshl" => "bvshl_cvc",
+                        "bvlshr" => "bvlshr_cvc",
+                        "bvashr" => "bvashr_cvc",
+                        "bvnand" => "bvnand_cvc",
+                        "bvnor" => "bvnor_cvc",
+                        "ehad" => "ehad_cvc",
+                        "arba" => "arba_cvc",
+                        "shesh" => "shesh_cvc",
+                        "smol" => "bvnor_cvc",
+                        "im" => "im_cvc",
+                    )
 
-                    expr = func * "(" * join(symbol_list[2:end], ", ") * ")" 
+                    expr = func * "(" * join(symbol_list[2:end], ", ") * ")"
 
                     line = "$(split_line[1]) = $(expr)"
                 end
-           end
+            end
         end
         push!(lines, line)
     end
