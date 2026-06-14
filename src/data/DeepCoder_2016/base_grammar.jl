84d44674e59bfce44b4a916ed5c542ae25304c7b
AlpiGun5 <a.i.guncan02@gmail.com>
2026-06-14 18:34:50 +0200

    refactor: simplify DeepCoder semantic spec

--- src/data/DeepCoder_2016/base_grammar.jl
+++ src/data/DeepCoder_2016/base_grammar.jl
@@ -3,22 +3,22 @@ base_grammar_deepcoder = @csgrammar begin
 
     ExprNum = Int
 
-    ExprNum = maximum(ExprArr)              := (length(x1) > 1, maximum(y) == maximum(x1), minimum(y) > minimum(x1))
-    ExprNum = minimum(ExprArr)              := (length(x1) > 1, maximum(y) < maximum(x1), minimum(y) == minimum(x1))
-    ExprNum = sum(ExprArr)                  := (length(x1) > 1)
-    ExprNum = first(ExprArr)                := (length(x1) > 1, maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), first(y) == first(x1), last(y) == first(x1))
-    ExprNum = last(ExprArr)                 := (length(x1) > 1, maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), first(y) == last(x1), last(y) == last(x1))
-    ExprNum = getindex(ExprArr, ExprNum)    := (length(x1) > 1, maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), first(x2) > 0, length(x1) > first(x2))
+    ExprNum = maximum(ExprArr)              := (maximum(y) == maximum(x1), minimum(y) >= minimum(x1))
+    ExprNum = minimum(ExprArr)              := (maximum(y) <= maximum(x1), minimum(y) == minimum(x1))
+    ExprNum = sum(ExprArr)
+    ExprNum = first(ExprArr)                := (maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), first(y) == first(x1), last(y) == first(x1))
+    ExprNum = last(ExprArr)                 := (maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), first(y) == last(x1), last(y) == last(x1))
+    ExprNum = getindex(ExprArr, ExprNum)    := (maximum(y) <= maximum(x1), minimum(y) >= minimum(x1))
 
-    ExprNum = countSt(ExprArr, Int)         := (length(x1) > 1, last(y) <= length(x1), last(y) >= 0)
-    ExprNum = countGt(ExprArr, Int)         := (length(x1) > 1, last(y) <= length(x1), last(y) >= 0)
-    ExprNum = countEq(ExprArr, Int)         := (length(x1) > 1, last(y) <= length(x1), last(y) >= 0)
-    ExprNum = countNeq(ExprArr, Int)        := (length(x1) > 1, last(y) <= length(x1), last(y) >= 0)
-    ExprNum = countMod(ExprArr, Int)        := (length(x1) > 1, last(y) <= length(x1), last(y) >= 0)
-    ExprNum = countNmod(ExprArr, Int)       := (length(x1) > 1, last(y) <= length(x1), last(y) >= 0)
+    ExprNum = countSt(ExprArr, Int)         := (y <= length(x1))
+    ExprNum = countGt(ExprArr, Int)         := (y <= length(x1))
+    ExprNum = countEq(ExprArr, Int)         := (y <= length(x1))
+    ExprNum = countNeq(ExprArr, Int)        := (y <= length(x1))
+    ExprNum = countMod(ExprArr, Int)        := (y <= length(x1))
+    ExprNum = countNmod(ExprArr, Int)       := (y <= length(x1))
 
-    ExprArr = drop(ExprArr, ExprNum)        := (length(y) < length(x1), maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), last(y) == last(x1), first(x2) > 0, length(x1) > maximum(x2))
-    ExprArr = take(ExprArr, ExprNum)        := (length(y) < length(x1), maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), first(y) == first(x1), first(x2) > 0, length(x1) > maximum(x2))
+    ExprArr = drop(ExprArr, ExprNum)        := (length(y) < length(x1), maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), last(y) == last(x1))
+    ExprArr = take(ExprArr, ExprNum)        := (length(y) < length(x1), maximum(y) <= maximum(x1), minimum(y) >= minimum(x1), first(y) == first(x1))
     ExprArr = sort(ExprArr)                 := (length(y) == length(x1), maximum(y) == maximum(x1), minimum(y) == minimum(x1), first(y) == minimum(x1), last(y) == maximum(x1))
     ExprArr = reverse(ExprArr)              := (length(y) == length(x1), maximum(y) == maximum(x1), minimum(y) == minimum(x1), first(y) == last(x1), last(y) == first(x1))
 
