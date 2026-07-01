c1d6a85317562d16aa7d3b90175e7f0b9de97864
AlpiGun5 <a.i.guncan02@gmail.com>
2026-07-01 14:02:27 +0200

    feat: add semantic annotations from Feng paper

--- src/data/Morpheus_2017/base_grammar.jl
+++ src/data/Morpheus_2017/base_grammar.jl
@@ -1,17 +1,74 @@
 base_grammar_morpheus = @csgrammar begin
     Start = Table
 
-    Table = gather(Table, NewCol, NewCol, ColSet) := (length(y) >= length(x1))
-    Table = spread(Table, Col, Col)               := (length(y) <= length(x1))
-    Table = unite(Table, NewCol, Col, Col)        := (length(y) == length(x1))
-    Table = separate(Table, Col, NewCol, NewCol)  := (length(y) == length(x1))
-    Table = select(Table, ColSet)                 := (length(y) == length(x1))
-    Table = group_by(Table, ColSet)               := (length(y) == length(x1))
-    Table = arrange(Table, ColSet)                := (length(y) == length(x1))
+    Table = gather(Table, NewCol, NewCol, ColSet) := (
+        morpheus_group_count(y) == morpheus_group_count(x1),
+        morpheus_new_value_count(y) <= morpheus_new_value_count(x1) + 2,
+        morpheus_new_column_count(y) <= morpheus_new_column_count(x1) + 2,
+        morpheus_row_count(y) >= morpheus_row_count(x1),
+        morpheus_column_count(y) <= morpheus_column_count(x1),
+    )
+    Table = spread(Table, Col, Col) := (
+        morpheus_group_count(y) == morpheus_group_count(x1),
+        morpheus_new_value_count(y) <= morpheus_new_value_count(x1),
+        morpheus_new_column_count(y) <= morpheus_new_value_count(x1),
+        morpheus_row_count(y) <= morpheus_row_count(x1),
+        morpheus_column_count(y) >= morpheus_column_count(x1),
+    )
+    Table = unite(Table, NewCol, Col, Col) := (
+        morpheus_group_count(y) == morpheus_group_count(x1),
+        morpheus_new_value_count(y) >= morpheus_new_value_count(x1) + 1,
+        morpheus_new_column_count(y) <= morpheus_new_column_count(x1) + 1,
+        morpheus_row_count(y) == morpheus_row_count(x1),
+        morpheus_column_count(y) == morpheus_column_count(x1) - 1,
+    )
+    Table = separate(Table, Col, NewCol, NewCol) := (
+        morpheus_group_count(y) == morpheus_group_count(x1),
+        morpheus_new_value_count(y) >= morpheus_new_value_count(x1) + 2,
+        morpheus_new_column_count(y) <= morpheus_new_column_count(x1) + 2,
+        morpheus_row_count(y) == morpheus_row_count(x1),
+        morpheus_column_count(y) == morpheus_column_count(x1) + 1,
+    )
+    Table = select(Table, ColSet) := (
+        morpheus_group_count(y) == morpheus_group_count(x1),
+        morpheus_new_value_count(y) <= morpheus_new_value_count(x1),
+        morpheus_new_column_count(y) <= morpheus_new_column_count(x1),
+        morpheus_row_count(y) == morpheus_row_count(x1),
+        morpheus_column_count(y) < morpheus_column_count(x1),
+    )
+    Table = group_by(Table, ColSet) := (
+        morpheus_group_count(y) >= morpheus_group_count(x1),
+        morpheus_new_value_count(y) == morpheus_new_value_count(x1),
+        morpheus_new_column_count(y) == morpheus_new_column_count(x1),
+        morpheus_row_count(y) == morpheus_row_count(x1),
+        morpheus_column_count(y) == morpheus_column_count(x1),
+    )
+    Table = arrange(Table, ColSet) := (length(y) == length(x1))
 
-    Table = filter(Table, Pred)                   := (length(y) <= length(x1))
-    Table = summarise(Table, NewCol, Agg)         := (length(y) <= length(x1))
-    Table = mutate(Table, NewCol, ValExpr)        := (length(y) == length(x1))
+    Table = filter(Table, Pred) := (
+        morpheus_group_count(y) == morpheus_group_count(x1),
+        morpheus_new_value_count(y) <= morpheus_new_value_count(x1),
+        morpheus_new_column_count(y) == morpheus_new_column_count(x1),
+        morpheus_row_count(y) < morpheus_row_count(x1),
+        morpheus_column_count(y) == morpheus_column_count(x1),
+    )
+    Table = summarise(Table, NewCol, Agg) := (
+        morpheus_group_count(y) == morpheus_group_count(x1),
+        morpheus_group_count(y) == morpheus_row_count(y),
+        morpheus_new_value_count(y) <= morpheus_new_value_count(x1) + morpheus_group_count(x1) + 1,
+        morpheus_new_column_count(y) > 0,
+        morpheus_new_column_count(y) <= morpheus_new_column_count(x1) + 1,
+        morpheus_row_count(y) <= morpheus_row_count(x1),
+        morpheus_column_count(y) <= morpheus_column_count(x1) + 1,
+    )
+    Table = mutate(Table, NewCol, ValExpr) := (
+        morpheus_group_count(y) == morpheus_group_count(x1),
+        morpheus_new_column_count(y) == morpheus_new_column_count(x1) + 1,
+        morpheus_new_value_count(x1) < morpheus_new_value_count(y),
+        morpheus_new_value_count(y) <= morpheus_new_value_count(x1) + morpheus_row_count(x1),
+        morpheus_row_count(y) == morpheus_row_count(x1),
+        morpheus_column_count(y) == morpheus_column_count(x1) + 1,
+    )
     Table = inner_join(Table, Table)
 
     ColSet = cols(Col)
@@ -34,9 +91,12 @@ base_grammar_morpheus = @csgrammar begin
     Pred = gt_value(Col, Value)
 
     ValExpr = copy_expr(Col)
-    ValExpr = div_expr(Col, Col)
+    ValExpr = div_expr(Col, Col) := (x1 != x2)
     ValExpr = div_sum_expr(Col)
-    ValExpr = add_expr(Col, Col)
-    ValExpr = sub_expr(Col, Col)
-    ValExpr = mul_expr(Col, Col)
+    ValExpr = add_expr(Col, Col) := (x1 != x2)
+    ValExpr = sub_expr(Col, Col) := (x1 != x2)
+    ValExpr = mul_expr(Col, Col) := (x1 != x2)
+
+    Value = lit(0)
+    Value = lit(1)
 end
