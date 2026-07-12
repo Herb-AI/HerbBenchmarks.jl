18da6aa01affe9cf4324d40f4a120c8e04470fbe
AlpiGun5 <a.i.guncan02@gmail.com>
2026-07-12 04:56:37 +0200

    feat: add a make_interpreter

--- src/data/Morpheus_2017/Morpheus_2017.jl
+++ src/data/Morpheus_2017/Morpheus_2017.jl
@@ -2,11 +2,19 @@ module Morpheus_2017
 using HerbCore
 using HerbSpecification
 using HerbGrammar
+using HerbInterpret
+
+using RuntimeGeneratedFunctions
+RuntimeGeneratedFunctions.init(@__MODULE__)
 
 include("table_primitives.jl")
 include("data.jl")
 include("table_functions.jl")
 include("base_grammar.jl")
 include("grammars.jl")
+
+function make_morpheus_interpreter(g)
+    return make_interpreter(g; target_module=Morpheus_2017, cache_module=Morpheus_2017)
+end
 
 end # module Morpheus_2017
